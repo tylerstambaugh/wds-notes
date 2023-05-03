ddc006d0a251f71011d928c8292ec569312db2bc
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-05-02 20:53:13 -0400

    annoying dog

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,62 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
 function App() {
-  return <h1>hi</h1>;
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>show</h1>} />
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
