2a215049af6613618763b9869f5030a7c51e8335
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-05-08 20:51:48 -0400

    more demos

--- src/hooksDemo/HooksDemo.tsx
+++ src/hooksDemo/HooksDemo.tsx
@@ -1,12 +1,16 @@
 import { UseRefDemo } from "./UseRefDemo";
 import { Counter } from "./UseStateDemo";
+import { Form } from "./referentialEqualityDemo";
 
 export function HooksDemo() {
   return (
     <>
       <h1>Dealing with hooks</h1>
       <UseRefDemo />
+      <hr />
       <Counter />
+      <hr />
+      <Form />
     </>
   );
 }
--- src/hooksDemo/UseStateDemo.tsx
+++ src/hooksDemo/UseStateDemo.tsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 export function Counter() {
   const [count, setCount] = useState(0);
 
+  //don't use this b/c count isn't current:
+  //   function adjustCount2(amount: number) {
+  //     setCount(count + amount)
+  //   }
+
+  //use the setCount method version instead.
   function adjustCount(amount: number) {
     setCount((currentCount) => {
       return currentCount + amount;
@@ -12,6 +18,7 @@ export function Counter() {
   return (
     <>
       <button onClick={() => adjustCount(-1)}>-</button>
+      {count}
       <button onClick={() => adjustCount(1)}>+</button>
     </>
   );
